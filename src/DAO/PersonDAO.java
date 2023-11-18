810567bf24985837d6b3a6265ff9c153fda9facf
willian-vieira <williannvieira.w@gmail.com>
2023-11-18 10:48:40 -0300

    feat: Lista todas as Pessoas retornadas do Banco de Dados

--- src/DAO/PersonDAO.java
+++ src/DAO/PersonDAO.java
@@ -1,13 +1,51 @@
 package DAO;
 
+import Factory.ConnectionFactory;
 import Model.Person;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDAO implements IPersonDAO {
+
+    /**
+     * Lista Todas as Pessoas
+     * @return listPerson
+     */
     @Override
     public List<Person> listPerson() {
-        return null;
+        List<Person> listPerson = new ArrayList<>();
+        Person person = null;
+        ResultSet resultSet = null;
+        PreparedStatement pstm = null;
+        Connection connection = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            pstm = connection.prepareStatement("SELECT * FROM Crud");
+            resultSet = pstm.executeQuery();
+
+            while (resultSet.next()) {
+                person = new Person();
+                person.setId(resultSet.getLong(1));
+                person.setName(resultSet.getString(2));
+                person.setCpf(resultSet.getString(3));
+                person.setEmail(resultSet.getString(4));
+                person.setPhone(resultSet.getString(5));
+                listPerson.add(person);
+            }
+
+            connection.close();
+            pstm.close();
+            resultSet.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listPerson;
     }
 
     @Override
